66074ef99f50eaa8a728347e531c15137e530f42
ShaneMcDev43 <150823885+ShaneMcDev43@users.noreply.github.com>
2026-05-22 14:23:28 +0100

    Load model weights manually for deployment

--- src/predict.py
+++ src/predict.py
@@ -1,17 +1,43 @@
 import pickle
 
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Bidirectional, Dense, Dropout, SpatialDropout1D
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 from src.preprocess import clean_text
 
 
+MAX_WORDS = 20000
 MAX_LEN = 50
-MODEL_PATH = "sentiment_model.h5"
+WEIGHTS_PATH = "sentiment_weights.weights.h5"
 TOKENIZER_PATH = "models/tokenizer.pkl"
 
 
-model = load_model(MODEL_PATH)
+def build_bilstm_model():
+    model = Sequential([
+        Embedding(input_dim=MAX_WORDS, output_dim=128, input_length=MAX_LEN),
+        SpatialDropout1D(0.2),
+        Bidirectional(LSTM(64, dropout=0.2, recurrent_dropout=0.2)),
+        Dense(32, activation="relu"),
+        Dropout(0.3),
+        Dense(1, activation="sigmoid")
+    ])
+
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer="adam",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+
+model = build_bilstm_model()
+
+# Build model before loading weights
+model.build(input_shape=(None, MAX_LEN))
+
+model.load_weights(WEIGHTS_PATH)
 
 with open(TOKENIZER_PATH, "rb") as f:
     tokenizer = pickle.load(f)
